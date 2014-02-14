f4b4402ef4ab7f306966749bdc13a7b30dbb29bb
Chih-Hsuan Huang <gemhuang2151992@gmail.com>
2014-02-14 13:26:15 -0700

    Add classes: 	-Iventory 	-Item 	-Users

---
+++ src/main/java/FoodLink/Inventory.java
@@ -0,0 +1,22 @@
+package FoodLink;
+
+public class Inventory{
+	
+	
+	public void addItem(Item item){
+		
+	}
+	
+	public void removeItem(int itemID){
+		
+	}
+	
+	public Item getItem(){
+		//TODO 
+		return null;
+	}
+	
+	public void priceComparison(){
+		
+	}
+}
---
+++ src/main/java/FoodLink/Item.java
@@ -0,0 +1,33 @@
+package FoodLink;
+
+public class Item {
+	private int itemKey;
+	private int itemQuantity;
+	private double itemPrice;
+	private String itemName;
+
+	public int getItemKey() {
+		return itemKey;
+	}
+	public void setItemKey(int itemKey) {
+		this.itemKey = itemKey;
+	}
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+	public double getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(double itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}		
+}
