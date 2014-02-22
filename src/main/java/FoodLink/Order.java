719ccaaed2a9c6ad834e90c3af582ebd7ccf4e2c
huynhj <kari14872@gmail.com>
2014-02-21 23:40:07 -0700

    re-commit

--- src/main/java/FoodLink/Order.java
+++ src/main/java/FoodLink/Order.java
@@ -1,10 +1,21 @@
 package FoodLink;
 
 public class Order {
+	Order invoices;
+	
+	public void createOrder(int storeID){
+		//Item aItem = new Item();
+	}
 	public void viewInvoices(){
 		
+		
+	}
+	
+	public Order getInvoices(int referenceNumber){
+		return invoices;
 	}
 
 	public void printInvoices(){
+
 	}
 }
