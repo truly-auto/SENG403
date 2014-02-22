4e539e747f415205f87e1206ca5f0545eaa3330c
huynhj <kari14872@gmail.com>
2014-02-21 22:30:45 -0700

    invoice update

--- src/main/java/FoodLink/Order.java
+++ src/main/java/FoodLink/Order.java
@@ -1,16 +1,8 @@
 package FoodLink;
 
 public class Order {
-	Order invoices;
-	
-	public void createOrder(int storeID){
-		Item aItem = new Item();
+	public void viewInvoices(){
 		
-		
-	}
-	
-	public Order getInvoices(int referenceNumber){
-		return invoices;
 	}
 
 	public void printInvoices(){
