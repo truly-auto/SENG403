ec3ea3400e59418fd12242e6c440a2169ac814f0
Lisa <lisa_jacobe@yahoo.com>
2014-03-15 11:50:25 -0600

    fixed the cancel order button

--- src/main/java/FoodLink/gui/SupermarketSys.java
+++ src/main/java/FoodLink/gui/SupermarketSys.java
@@ -184,14 +184,10 @@ public class SupermarketSys {
 
 				//This codes will create a new tab called NEW ORDER
 				UIManager.put("newOrder.selected",ColorUIResource.GREEN);
-
-				//This codes will create a new tab called NEW ORDER #
-				
-
 				final JPanel newOrder = new JPanel();
-				tabNumber++;
-				String tabName = "NEW ORDER " + tabNumber;
-				mainTabbedPane.addTab(tabName, null, newOrder, null);		
+				//tabNumber++;
+				//String tabName = "NEW ORDER " + tabNumber;
+				mainTabbedPane.addTab("NEW ORDER", null, newOrder, null);		
 				mainTabbedPane.setBackgroundAt(4, new Color(0, 128, 0));
 				mainTabbedPane.setForegroundAt(4, new Color(255, 255, 255));
 				GridBagLayout gbl_newOrder = new GridBagLayout();
@@ -342,6 +338,8 @@ public class SupermarketSys {
 				gbc_btnNewButton_3.gridy = 11;
 				newOrder.add(btnNewButton_3, gbc_btnNewButton_3);
 
+				
+
 			}
 		});
 		GridBagConstraints gbc_btnNewButton_1 = new GridBagConstraints();
@@ -417,7 +415,7 @@ public class SupermarketSys {
 
 		
 		//CODES FOR NEW ORDER PAGE 
-		
+
 
 		
 
