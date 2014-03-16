5dc73f8ecb8edbeb8ce36871b851b9fab2ca76f1
huynhj <kari14872@gmail.com>
2014-03-15 22:01:59 -0600

    created empty supermarket test class

---
+++ src/test/java/FoodLink/TestSupermarketSys.java
@@ -0,0 +1,16 @@
+package FoodLink;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import FoodLink.gui.SupermarketSys;
+
+public class TestSupermarketSys {
+
+	@Test
+	public void test() {
+		//tests
+	}
+
+}
