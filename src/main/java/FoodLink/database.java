c31d5dd43badceae4989eda92ec78e17adcb501c
Lisa <lisa_jacobe@yahoo.com>
2014-04-07 21:58:24 -0600

    removed some extra lines

--- src/main/java/FoodLink/database.java
+++ src/main/java/FoodLink/database.java
@@ -824,7 +824,6 @@ public class database {
 			         System.out.println("The last INVOICE_NUMBER: " + invoiceNum);
 			     }
 		    
-		     
 			}
 		catch (SQLException e) {
 		     e.fillInStackTrace();
