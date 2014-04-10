e628046ec1db84f5d9a6f4230ccc71ddc7f9c8a6
ericckk <kingem43@hotmail.com>
2014-04-10 16:48:54 -0600

    fixing the supplier

--- src/main/java/FoodLink/gui/SupplierSys.java
+++ src/main/java/FoodLink/gui/SupplierSys.java
@@ -16,7 +16,6 @@ import java.awt.Dialog.ModalityType;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Frame;
 import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -59,10 +58,15 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+
 import javax.swing.JToolBar;
+
 import java.awt.Button;
 import java.awt.Panel;
+
 import javax.swing.JSplitPane;
+
+import com.sun.java.swing.plaf.windows.resources.windows;
 
 public class SupplierSys {
 
@@ -77,13 +81,9 @@ public class SupplierSys {
 	private JTable table_1;
 	private JTable inventoryTable;
 	private JTable userTable;
-	private final JScrollPane scrollPane_2 = new JScrollPane();
-	private JTable table_3;
-	private JTable orderStatusTable;
-	private GradientButton btnViewInvoice = new GradientButton("View Order");
+	private final JScrollPane scrollPane_2 = new JScrollPane();	
 	private JTable commentsTable;
-
-	/**
+		/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
@@ -91,7 +91,7 @@ public class SupplierSys {
 			public void run() {
 				try {
 					//hard code parameter to switch suppliers here (1-5)
-					SupplierSys window = new SupplierSys(2, true);
+					SupplierSys window = new SupplierSys(1, true);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -116,11 +116,11 @@ public class SupplierSys {
 		supplier = connect.getSpecSupplier(supplier_id);
 		System.out.println("this is .." + supplier_id);
 		frame = new JFrame();
-		LookAndFeel lookAndFeel = new LookAndFeel(frame);	
-		frame.setTitle("FoodLink");		
-		frame.setLocation(100, 100);
-		frame.setSize(new Dimension(1000,600));
-	    frame.setExtendedState(Frame.MAXIMIZED_BOTH); //MT: Sets frame to open to maximized 
+		LookAndFeel lookAndFeel = new LookAndFeel(frame);
+		//frame.setSize(new Dimension(1000,1000));
+		frame.setTitle("FoodLink");
+		//frame.setBounds(100, 100, 608, 300);
+		frame.setBounds(100, 100, 640, 420);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
@@ -141,7 +141,7 @@ public class SupplierSys {
 		banner.setBackground(green);
 		frame.getContentPane().add(banner);
 		frame.setBackground(green);
-		//frame.setSize(new Dimension(1000,700));
+		frame.setSize(new Dimension(1000,700));
 		
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -242,8 +242,6 @@ public class SupplierSys {
 		orderTab.add(scrollPane, gbc_scrollPane);
 		
 		table = new JTable();
-		table.getTableHeader().setReorderingAllowed(false);
-
 		scrollPane.setViewportView(table);
 		
 		JPanel inventoryTab = new JPanel();
@@ -262,13 +260,14 @@ public class SupplierSys {
 		
 		final String[] columnNames = {"Item Number", "Item name", "Type", "Quantity", "Unit Price", "Units"};
 		
-		//@Production <--Nice tag to search for when swapping this code before running code
 		//this one will access data from the the database but will cause the code not to work in design mode
 		//use this one when testing
-		final Object [][] data = connect.getInventory(supplier_id);
+		//
+		final Object[][] data = connect.getInventory(supplier_id);
 		
 		//use this one when building
 		//final Object [][] data = {{"1","papples", "fruits", "5000", "2000", "100 lb"},{"2","apples", "fruits", "5000", "2000", "200 lb"},{"3","grapes", "fruits", "5000", "2000", "40 lb"},{"4","pears", "fruits", "5000", "2000", "lb"} };
+		
 		
 		final JScrollPane scrollPane_1 = new JScrollPane();
 		GridBagConstraints gbc_scrollPane_1 = new GridBagConstraints();
@@ -279,7 +278,6 @@ public class SupplierSys {
 		inventoryTab.add(scrollPane_1, gbc_scrollPane_1);
 		
 		inventoryTable = new JTable(data, columnNames);
-		inventoryTable.getTableHeader().setReorderingAllowed(false);
 		inventoryTable.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent mevt) {
@@ -304,7 +302,6 @@ public class SupplierSys {
 					Object [] [] data2 = connect.getInventory(supplier_id);
 					//creating new table with the new data from the database
 					inventoryTable = new JTable(data2, columnNames);
-					inventoryTable.getTableHeader().setReorderingAllowed(false);
 					//adding action listener on the new table 
 					inventoryTable.addMouseListener(new MouseAdapter() {
 						@Override
@@ -353,7 +350,6 @@ public class SupplierSys {
 					Object [] [] data2 = connect.getInventory(supplier_id);
 					//creating a new table with new information
 					inventoryTable = new JTable(data2, columnNames);
-					inventoryTable.getTableHeader().setReorderingAllowed(false);
 					//adding an action listener
 					inventoryTable.addMouseListener(new MouseAdapter() {
 						@Override
@@ -393,7 +389,6 @@ public class SupplierSys {
 						Object [] [] data2 = connect.getInventory(supplier_id);
 						//adding the information into a new table
 						inventoryTable = new JTable(data2, columnNames);
-						inventoryTable.getTableHeader().setReorderingAllowed(false);
 						//adding an action listener to the table
 						inventoryTable.addMouseListener(new MouseAdapter() {
 							@Override
@@ -445,116 +440,205 @@ public class SupplierSys {
 		gbc_supermarketScroller.gridy = 0;
 		supermarketTab.add(supermarketScroller, gbc_supermarketScroller);
 		
-		String[] commentColumnNames = {"name", "comment", "date [yy-mm-dd]"};
-		
-		//@Production <--Nice tag to search for when swapping this code before running code
-		//When building the gui
-		Object[][] commentData = {{"This", "Is a comment", "1991-01-02"}};
-		//When running the program
-		//Object[][] commentData = connect.getSuplierComments(supplier_id);
-		
-		commentsTable = new JTable(commentData, commentColumnNames);
-		commentsTable.getTableHeader().setReorderingAllowed(false);
+		String[] commentColumnNames = {"name", "comment"};
+		commentsTable = new JTable(connect.getSuplierComments(supplier_id), commentColumnNames);
+		//commentsTable = connect.getSuplierComments(supplier_id)
 		supermarketScroller.setViewportView(commentsTable);
 		
 		JPanel accountTab = new JPanel();
 		mainTabbedPane.addTab("Account", null, accountTab, null);
-
-		/* Invoice tab */
 		
-		final Object[][] orderList = connect.getOrderListSupplier(supplier_id);
 		
-
-		JPanel jpInvoices = new JPanel();
-		mainTabbedPane.addTab("Invoices", null, jpInvoices, null);
-		GridBagLayout gbl_jpInvoices = new GridBagLayout();
-		gbl_jpInvoices.columnWidths = new int[] { 0, 0 };
-		gbl_jpInvoices.rowHeights = new int[] { 0, 0, 0 };
-		gbl_jpInvoices.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-		gbl_jpInvoices.rowWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
-		jpInvoices.setLayout(gbl_jpInvoices);
-
-		JScrollPane scrollPane_3 = new JScrollPane();
-		GridBagConstraints gbc_scrollPane_3 = new GridBagConstraints();
-		gbc_scrollPane_3.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane_3.gridx = 0;
-		gbc_scrollPane_3.gridy = 1;
-		jpInvoices.add(scrollPane_3, gbc_scrollPane_3);
-
-		btnViewInvoice.setEnabled(false);
-		btnViewInvoice.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("Clicked the View Order Button");
-
-				int row = orderStatusTable.getSelectedRow();
-
-				int invoiceNum = Integer.parseInt((String) orderStatusTable
-						.getValueAt(row, 0));
-				System.out.println("The invoice number passed is: "
-						+ invoiceNum);
-
-				String supermarketName = (String) orderStatusTable.getValueAt(row, 1);
-				String dateTime = (String) orderStatusTable.getValueAt(row, 3);
-				String grandTotal1 = (String) orderStatusTable.getValueAt(row,
-						2);
-				String status = (String) orderStatusTable.getValueAt(row, 4);
-
-				String store_id = (String)orderList[orderStatusTable.getSelectedRow()][5];
-				
-				ViewInvoice viewInvoice = new ViewInvoice(invoiceNum,
-						supermarketName, store_id,supplier_id,dateTime, status, grandTotal1, row,
-						orderStatusTable);
-				viewInvoice.setVisible(true);
-			}
-		});
-
-		JButton btnNewButton_4 = new JButton("View Invoice");
-		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
-		gbc_btnNewButton_4.anchor = GridBagConstraints.WEST;
-		gbc_btnNewButton_4.insets = new Insets(0, 0, 5, 0);
-		gbc_btnNewButton_4.gridx = 0;
-		gbc_btnNewButton_4.gridy = 0;
-		jpInvoices.add(btnViewInvoice, gbc_btnNewButton_4);
-
+		/*Invoice tab*/	
+		JPanel jpInvoices = new JPanel(new BorderLayout());
+		mainTabbedPane.addTab("Invoices",null, jpInvoices, null);
 		
-		final String[] columnTitle = new String[] { "Invoice Number",
-				"Supermarket", "Total Cost($)", "Date/Time Created", "Status"};
-
-		DefaultTableModel orderModel = new DefaultTableModel(orderList,
-				columnTitle) {
-			boolean[] columnEditables = new boolean[] { false, false, false,
-					false };
-		public boolean isCellEditable(int row, int column) {
-				return columnEditables[column];
-			}
+		final ArrayList<Object[][]> listOrders = new ArrayList<Object[][]>();
+		
+		final Object[][] order1 = new Object[][]{
+				{data[0][0], data[0][1], 2, data[0][4], 2 * Double.parseDouble((String) data[0][4])},
+				{data[1][0], data[1][1], 2, data[1][4], 2 * Double.parseDouble((String) data[1][4])}
 		};
-		;
-		orderStatusTable = new JTable(orderModel);
-		orderStatusTable.getTableHeader().setReorderingAllowed(false);
-		orderStatusTable.addMouseListener(new MouseAdapter() {
+		
+		
+		final Object[][] order2 = new Object[][]{
+				{data[0][0], data[0][1], 20, data[0][4], 20 * Double.parseDouble((String) data[0][4])},
+				{data[1][0], data[1][1], 12, data[1][4], 12 * Double.parseDouble((String) data[1][4])},
+				{data[3][0], data[3][1], 4, data[3][4], 4 * Double.parseDouble((String) data[3][4])}
+		};
+		
+		listOrders.add(order1);
+		listOrders.add(order2);
+		
+		final String[] title = new String[] {"Item ID", "Item", "quantity", "Price($)", "Total($)"};
+		final JTable jtInvoice = new JTable();
+
+		jtInvoice.getTableHeader().setReorderingAllowed(false);
+		final JComboBox jcbSupermarkets = new JComboBox(new String[] {"supermarket1","supermarket2"});
+		jcbSupermarkets.setSelectedIndex(-1);
+
+		//final JComboBox jcbSupermarkets = new JComboBox(new String[] {"supermarket1","supermarket2"});
+
+		jcbSupermarkets.addActionListener(new ActionListener() {
+			
+			@SuppressWarnings("serial")
 			@Override
-			public void mouseClicked(MouseEvent e) {
-				System.out.println("Clicking Table");
-				btnViewInvoice.setEnabled(true);
+			public void actionPerformed(ActionEvent arg0) {
+				int index = jcbSupermarkets.getSelectedIndex();
+				jtInvoice.setModel(new DefaultTableModel(listOrders.get(index),title) {
+					boolean[] columnEditables = new boolean[] { false, false, false,
+							false };
+
+					public boolean isCellEditable(int row, int column) {
+						return columnEditables[column];
+					}
+				});
 			}
 		});
-
-		scrollPane_3.setViewportView(orderStatusTable);
-
+		
+		GradientButton jbPrint = new GradientButton("Save invoice and Open");
+		jbPrint.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if(jcbSupermarkets.getSelectedIndex() >= 0){
+						File file = new File("invoice_" + jcbSupermarkets.getSelectedItem()+ ".xls");
+						TableModel model = jtInvoice.getModel();
+						FileWriter out = new FileWriter(file);
+						SimpleDateFormat sdf = new SimpleDateFormat("MMM. d, yyyy", java.util.Locale.ENGLISH);
+						Double totalPrice = 0.0;
+						
+						out.write(supplier[0] + "\t\t\t" + jcbSupermarkets.getSelectedItem() + "\n");
+						out.write(supplier[1] + "\t\t\t" + "SUPERMARKET ADDRESS" + "\n");
+						out.write(supplier[2] + "\t\t\t" + "SUPERMARKET PHONE#" + "\n");
+						out.write(supplier[3] + "\t\t\t" + sdf.format(java.util.Calendar.getInstance().getTime()) + "\n");
+						out.write("\n\n");
+						
+						for(int i = 0; i < model.getColumnCount(); i++){
+							out.write(model.getColumnName(i) + "\t");
+						}
+						
+						out.write("\n");
+						for(int i=0; i < model.getRowCount();i++){
+							for(int j=0;j < model.getColumnCount();j++){
+								if (j == 4){
+									totalPrice += (Double) model.getValueAt(i, j);
+								}
+								out.write(model.getValueAt(i,j).toString() + "\t");
+							}
+								out.write("\n");
+						}
+						
+						out.write("\n");
+						out.write("\t\t\tTOTAL:\t" + totalPrice + "\n");
+						out.close();
+						
+						String osname = System.getProperty("os.name").toLowerCase();
+						Runtime r = Runtime.getRuntime();
+						if(osname.contains("win")){
+							r.exec("cmd.exe /c start " + file);
+						} else if (osname.contains("mac") || osname.contains("linux")){
+							r.exec("open "+ file);
+						} else{
+							JOptionPane.showMessageDialog(frame, "Operating System not supported for printing");
+						}						
+						/*THE FOLLOWING LINE PRINTS OFF THE TABLE DIRECTLY*/
+						//jtInvoice.print(JTable.PrintMode.NORMAL);
+					}
+					else{
+						JOptionPane.showMessageDialog(frame, "Please select an invoice to print");
+					}
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		
+		JScrollPane jspInvoice = new JScrollPane(jtInvoice);
+		jpInvoices.add(jcbSupermarkets, BorderLayout.NORTH);
+		jpInvoices.add(jspInvoice, BorderLayout.CENTER);
+		JPanel jpSouth = new JPanel(new FlowLayout());
+		jpSouth.add(jbPrint);
+		jpInvoices.add(jpSouth,BorderLayout.SOUTH);
+		
+		if(manager){	
+			JToolBar toolBar = new JToolBar();
+			mainTabbedPane.addTab("Users", null, toolBar, null);
+			
+			JPanel panel_1 = new JPanel();
+			toolBar.add(panel_1);
+			
+			Button button_2 = new Button("Delete User");
+			panel_1.add(button_2);
+			
+			Button button = new Button("Add User");
+			panel_1.add(button);
+			
+			toolBar.add(scrollPane_2);
+			
+			final String[] users = {"User name", "Privileges"};
+		
+			setTable(users, supplier_id);
+			
+			button.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					String [] user = null;
+					try {
+						AddUser window = new AddUser(supplier_id);
+						/**
+						 * Poorly coded to avoid having to refactor this whole class for now
+						 * (Instead of turning into a JDialog, making JDialog inherit conentpane of frame)
+						 */
+						window.setContentPane(window.frame.getContentPane());
+						window.setSize(new Dimension(500, 300));
+						
+						window.setModalityType(ModalityType.APPLICATION_MODAL);	
+						//window.frame.setModalExclusionType(Dialog.ModalExclusionType.APPLICATION_EXCLUDE);
+						//window.frame.setVisible(true);
+						window.setVisible(true);
+						user = window.getResult();
+						connect.manageSupplierUsers(user, supplier_id, true);
+						window.setVisible(false);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+					
+					
+					if(user[0]!=null)	
+						{//resetting the table
+							setTable(users, supplier_id);
+						}
+					
+					}
+				});
+				
+				button_2.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						System.out.println("about to delete this row.."+ selectedUser);
+						String [] user = {selectedUser};
+						//passing the user name, the supplier_id and false to trigger the delete query
+						connect.manageSupplierUsers(user, supplier_id, false);
+						setTable(users, supplier_id);
+					}
+		
+				
+				});	
+		
+		}
 	}
 
+
 	protected void setTable(String [] users, int supplier_id) {
-		
-		//@Production <--Nice tag to search for when swapping this code before running code
 		//use this one when testing
+		//
 		final Object[][] userData = connect.getUser(supplier_id, false);
 				
 		//use this one when building
 		//final Object [][] userData = {{"Josh", "true" },{"Tom", "false" },{"Jayceon", "true" },{"J-Mello", "false" } };
 				
 		userTable = new JTable(userData, users);
-		userTable.getTableHeader().setReorderingAllowed(false);
 		userTable.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent mevt) {
